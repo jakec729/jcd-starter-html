3c11ba3396d636d872c30dbae3bab19c188fe42a
Jake Cooper <jakec729@mac.com>
2015-09-05 14:04:25 -0400

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp = require('gulp');
+var	sass = require('gulp-sass');
+var	rename = require('gulp-rename');
+var	autoprefixer = require('gulp-autoprefixer');
+var	plumber = require('gulp-plumber');
+var	browserSync = require('browser-sync');
+var jade = require('gulp-jade');
+
+
+// Static server
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+
+gulp.task('sass', function () {
+    gulp.src('./sass/**/*.scss')
+        .pipe(plumber())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream());
+ });
+
+
+gulp.task('jade', function() {
+    gulp.src('./views/*.jade')
+        .pipe(plumber())
+        .pipe(jade({ pretty: true }))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('default', ['watch']);
+
+gulp.task('watch', ['sass', 'jade'], function(){
+
+	browserSync.init({
+	    server: {
+	        baseDir: "./"
+	    }
+	});
+
+    gulp.watch('sass/**/*.scss', ['sass']);
+	gulp.watch('views/**/*.jade', ['jade']);
+	gulp.watch("**/*.html").on('change', browserSync.reload);
+});
